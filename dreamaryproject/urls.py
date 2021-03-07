047358dcbc1eb5e0638d60362d2c19ca2388ed2b
yuna0102 <mkiyg@naver.com>
2021-03-07 18:28:22 +0900

    django setting to static

--- dreamaryproject/urls.py
+++ dreamaryproject/urls.py
@@ -20,4 +20,5 @@ from page import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name="home"),
+    path('introduce/', views.introduce, name="introduce"),
 ]
--- page/views.py
+++ page/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
+
+def introduce(request):
+    return render(request, 'introduce.html')
